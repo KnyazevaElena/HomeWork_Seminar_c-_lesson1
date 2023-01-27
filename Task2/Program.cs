af0222953b4f0418332d71855f3a4b38cf2725c1
Elena Knyazeva <lenen241989sc@inbox.ru>
2023-01-28 01:25:15 +0700

    Поиск максимального числа

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
+Console.Clear();
+Console.Write ("Введите число: ");
+int Number1 = int.Parse (Console.ReadLine());
+Console.Write ("Введите второе число: ");
+int Number2 = int.Parse (Console.ReadLine());
+if(Number1 > Number2){
+    Console.WriteLine ($"{Number1} = max");
+}
+if (Number1 < Number2) {;
+    Console.WriteLine ($"{Number2} = max");
+}
